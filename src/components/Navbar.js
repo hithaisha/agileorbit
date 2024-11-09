38206f08f4103b83577276d0baa41afc5538afc5
Hithaisha Perera <hithaishaperera@Hithaishas-MacBook-Air.local>
2024-11-09 14:01:05 +0530

    changes new

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
-// src/components/Navbar.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import logo from '../assets/logo/agilelogo.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="navbar-logo">Agile Orbit</h1>
+      <div className="logo-container">
+        <img src={logo} alt="Company Logo" className="logo-image" />
+        <h1 className="navbar-logo">Agile Orbit</h1>
+      </div>
       <ul className="navbar-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
